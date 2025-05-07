c2e85981df9f0345687481f8ce9ac6c6354beef3
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-05-07 08:04:56 +0600

    Add files via upload

---
+++ Chapter 2 - Variables and Simple Data Types/Exercise/2.2.py
@@ -0,0 +1,12 @@
+"""
+2-2. Simple Messages: Assign a message to a variable, and print that message.
+Then change the value of the variable to a new message, and print the new
+message.
+
+"""
+
+message = "I am Tusher"
+print(message)
+
+message = "I am from Chandpur, Bangladesh"
+print(message)
