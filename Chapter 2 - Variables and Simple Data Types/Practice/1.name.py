cf82c2f31d2fa1615325b0dd7308264863af73c9
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-05-07 08:05:35 +0600

    Add files via upload

---
+++ Chapter 2 - Variables and Simple Data Types/Practice/1.name.py
@@ -0,0 +1,5 @@
+name = "ada lovelace"
+print(name.title())#The dot after name in name.tile() tells python to make the title() method act on the variable name
+
+print(name.upper())
+print(name.lower())
