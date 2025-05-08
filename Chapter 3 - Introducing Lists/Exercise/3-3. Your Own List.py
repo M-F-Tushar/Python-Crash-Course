27d2b7736ee4fec707d68bc78911b293995d7b76
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-05-09 00:05:58 +0600

    Add files via upload

---
+++ Chapter 3 - Introducing Lists/Exercise/3-3. Your Own List.py
@@ -0,0 +1,9 @@
+transportation = ["Tesla car", "Ducati motorcycle", "Yamaha scooter", "BMW electric bike"]
+
+print(f"I would like to own a {transportation[0]}")
+
+print(f"I would like to ride a {transportation[1]}")
+
+print(f"A {transportation[2]} is perfect for city travel")
+
+print(F"I think a {transportation[3]} would be fun and eco-friendly")
