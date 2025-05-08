80150cf5ba6d3cfaade33bb1266b78e08688035f
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-05-09 00:01:57 +0600

    Add files via upload

---
+++ Chapter 2 - Variables and Simple Data Types/Exercise/2-6. Famous Quote 2.py
@@ -0,0 +1,3 @@
+famous_person = "Tusher"
+message = f'According to {famous_person},"Some time life become so unbearable, but we shouldnot take that as a misfortune, but as a chance a shine even further" '
+print(message)
