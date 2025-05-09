2d6a3a8eca97220f5342217d245d8bf60e008b86
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-05-10 05:06:21 +0600

    Add files via upload

---
+++ Chapter 3 - Introducing Lists/Exercise/3-4. Guest List.py
@@ -0,0 +1,7 @@
+invite_dead = ['Uncle', 'Grandfather', 'Grandmother']
+
+print(f"I welcome you to for a dinner {invite_dead[0]}")
+
+print(f"I welcome you to for a dinner {invite_dead[1]}")
+
+print(f"I welcome you to for a dinner {invite_dead[2]}")
