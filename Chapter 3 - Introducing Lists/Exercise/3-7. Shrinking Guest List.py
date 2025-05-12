c0ce2468ce48af13d7f0e59fa2121ff06cee684c
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-05-12 21:20:14 +0600

    Add files via upload

---
+++ Chapter 3 - Introducing Lists/Exercise/3-7. Shrinking Guest List.py
@@ -0,0 +1,54 @@
+invite_dead = ['Uncle', 'Grandfather', 'Grandmother']
+
+print(f"I welcome you to for a dinner {invite_dead[0]}")
+
+print(f"I welcome you to for a dinner {invite_dead[1]}")
+
+print(f"I welcome you to for a dinner {invite_dead[2]}")
+
+unable_to_come = invite_dead.pop()
+
+print(f"Unfortunately {unable_to_come} is unable to come")
+
+invite_dead.append("Second Uncle")
+
+print(f"I welcome you to for a dinner {invite_dead[0]}")
+
+print(f"I welcome you to for a dinner {invite_dead[1]}")
+
+print(f"I welcome you to for a dinner {invite_dead[2]}")
+
+print("I found a bigger dinner table, so now i can invite more people")
+
+invite_dead.insert(0,"Third Uncle")
+invite_dead.insert(2,"Grandmother")
+invite_dead.append("Aunty")
+
+print(invite_dead)
+
+print("I am sorry, but i can only invite two people")
+
+unable_to_invite = invite_dead.pop()
+
+print(f"Sorry, I am unable to invite you {unable_to_invite}")
+
+unable_to_invite = invite_dead.pop()
+
+print(f"Sorry, I am unable to invite you {unable_to_invite}")
+
+unable_to_invite = invite_dead.pop()
+
+print(f"Sorry, I am unable to invite you {unable_to_invite}")
+
+unable_to_invite = invite_dead.pop()
+
+print(f"Sorry, I am unable to invite you {unable_to_invite}")
+
+print(f"But you are still invited {invite_dead[0]}")
+
+print(f"But you are still invited {invite_dead[1]}")
+
+del invite_dead[0]
+del invite_dead[1]
+
+print(invite_dead)
