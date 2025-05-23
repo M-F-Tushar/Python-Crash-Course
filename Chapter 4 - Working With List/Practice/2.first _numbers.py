28b77bd79b0ec1993f5f58ec7dcb3bed18ec6cd7
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-05-23 06:58:03 +0600

    Add files via upload

---
+++ Chapter 4 - Working With List/Practice/2.first _numbers.py
@@ -0,0 +1,8 @@
+for value in range(1,5):
+    print(value) # last number does not count
+
+for value in range(1, 6):
+    print(value)
+
+number = list(range(1,6)) #When you wrap around a list() call to the function, the output will be a list of numbers. range()
+print(number)
