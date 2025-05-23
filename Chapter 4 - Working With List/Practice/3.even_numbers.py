03f591c09cfcf54a509fbf0a62fbca1f68b89d6e
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-05-23 06:58:20 +0600

    Add files via upload

---
+++ Chapter 4 - Working With List/Practice/3.even_numbers.py
@@ -0,0 +1,2 @@
+even_numbers = list(range(2,11,2)) # the range() function starts with the value 2 and then adds 2 to that value. It adds 2 repeatedly until it reaches or passes the end value, 11
+print(even_numbers)
