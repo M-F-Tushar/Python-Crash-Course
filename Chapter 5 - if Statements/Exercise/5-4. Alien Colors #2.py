0c34ef4db7393815753347bc530c3684d5999549
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-05-24 20:45:04 +0600

    Add files via upload

---
+++ Chapter 5 - if Statements/Exercise/5-4. Alien Colors #2.py
@@ -0,0 +1,17 @@
+# Alien color is green
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("Player just earned 5 points for shooting the alien!")
+else:
+    print("Player just earned 10 points.")
+
+
+
+# Alien color is yellow
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print("Player just earned 5 points for shooting the alien!")
+else:
+    print("Player just earned 10 points.")
