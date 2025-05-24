51b63b0867c678fe2cd3a71130c8c02bb7b0766a
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-05-24 20:42:47 +0600

    Delete Chapter 5 - if Statements/Exercise/4.banned_users.py

--- Chapter 5 - if Statements/Exercise/4.banned_users.py
+++
@@ -1,6 +0,0 @@
-#Checking Whether a Value Is Not in a List
-banned_users = ['andrew', 'carolina', 'david']
-user = 'marie'
-
-if user not in banned_users:
-    print(f"{user.title()}, you can post a response if you wish.")
