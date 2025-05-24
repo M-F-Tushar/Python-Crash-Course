83ae67dd0ccc477783274481458663066e53a3b0
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-05-24 20:42:25 +0600

    Delete Chapter 5 - if Statements/Exercise/5.voting.py

--- Chapter 5 - if Statements/Exercise/5.voting.py
+++
@@ -1,8 +0,0 @@
-age = 19
-if age >=18:
-    print("You are old enough to vote!")
-    print("Have you registered to vote yet?")
-else:
-    print("SOrry, you are too young to vote.")
-    print("please register to vote as soon as you turn 18!")
-    
