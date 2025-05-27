dcf1006b6ecf4b1beebb9e4d0129de4ceec6eec8
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-05-27 13:04:46 +0600

    Add files via upload

---
+++ Chapter 7 - User Input and while Loops/Practice/6.parrot.py
@@ -0,0 +1,37 @@
+prompt = "\nTell me something and I will repeat it back at you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+message = ""
+while message != 'quit':
+    message = input(prompt)
+
+    if message != 'quit':
+        print(message)
+
+
+# Using a Flag
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program."
+
+active = True
+while active:
+    message = input(prompt)
+
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
+
+# Using break to Exit a Loop
+prompt = "\nPlease enter the name of a city you have visited:"
+prompt += "\n(Enter 'quit' when you are finished.) "
+
+while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print(f"I'd love to go to {city.title()}!")
+
+
